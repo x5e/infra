9c28849086751e2af558d3cb711ebb4e69517714
Darin McGill <darin.mcgill@gmail.com>
2017-12-28 03:22:57 -0500

    tweaks

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -120,8 +120,8 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_route_table" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+resource "aws_default_route_table" "main" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.main.id}"
