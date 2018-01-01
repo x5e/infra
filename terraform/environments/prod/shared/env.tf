ddafd7ccf8f1f2f97874bd50b4a545cf78eacc41
Darin McGill <darin.mcgill@gmail.com>
2018-01-01 00:03:30 -0500

    tweaks

--- terraform/environments/prod/shared/env.tf
+++ terraform/environments/prod/shared/env.tf
@@ -12,5 +12,6 @@ module "main" {
   disposable = "false"
   env_domain = "x5e.com"
   cert_arn = "arn:aws:acm:us-east-1:401701269211:certificate/835a1f34-1d56-4668-9d58-86794576331b"
+  trusted = ["69.162.169.108/32", "172.26.0.0/16"]
 }
 
