47aa207cb83e7328147dff1587c361598defbc47
Darin McGill <darin.mcgill@gmail.com>
2018-01-02 08:44:06 -0500

    tweaks

--- terraform/environments/qa/shared/env.tf
+++ terraform/environments/qa/shared/env.tf
@@ -14,6 +14,5 @@ module "shared" {
   env_domain = "x5e.qa"
   cert_arn = "arn:aws:acm:us-east-1:082367326120:certificate/4af129a1-9ca3-4c1b-8928-0e4a4896b1b2"
   disposable = true
-  trusted = ["69.162.169.108/32", "34.199.230.121/32"]
 }
 
--- terraform/modules/shared/security.tf
+++ terraform/modules/shared/security.tf
@@ -9,6 +9,13 @@ data external "ipify" {
 resource "aws_default_security_group" "default" {
   vpc_id = "${module.vpc.vpc_id}"
 
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 0
     to_port     = 65535
@@ -17,22 +24,20 @@ resource "aws_default_security_group" "default" {
     description = "within security group"
   }
 
-
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = "${var.trusted}"
-    description = "trusted"
+    cidr_blocks = "172.26.0.0/16"
+    description = "lightsail"
   }
 
-
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["${data.external.ipify.result.ip}/32"]
-    description = "terraforming computer"
+    cidr_blocks = "69.162.169.108/32"
+    description = "jabberwock"
   }
 
   ingress {
@@ -40,23 +45,6 @@ resource "aws_default_security_group" "default" {
     from_port = 22
     to_port   = 22
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  ingress {
-    protocol  = "tcp"
-    from_port = 3309
-    to_port   = 3309
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "tcp-ping utility"
-  }
-
-  ingress {
-    protocol  = "tcp"
-    from_port = 1234
-    to_port   = 1234
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow latency test to skip load balancer"
   }
 
   ingress {
@@ -67,10 +55,21 @@ resource "aws_default_security_group" "default" {
     description = "allow https from outside"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+  ingress {
+    protocol  = "tcp"
+    from_port = 3309
+    to_port   = 3309
     cidr_blocks = ["0.0.0.0/0"]
+    description = "tcp-ping utility"
   }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["${data.external.ipify.result.ip}/32"]
+    description = "terraforming computer"
+  }
+
+
 }
