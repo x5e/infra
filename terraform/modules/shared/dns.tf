adb7d1e84ca4fc55621b23382c59fbd3c94809e5
Darin McGill <darin.mcgill@gmail.com>
2018-01-03 09:22:10 -0500

    tweaks

--- terraform/modules/shared/dns.tf
+++ terraform/modules/shared/dns.tf
@@ -13,12 +13,3 @@ resource "aws_route53_record" "lb" {
   records = ["${aws_lb.main.dns_name}"]
 }
 
-
-resource "aws_route53_record" "region" {
-  zone_id = "${data.aws_route53_zone.env_zone.zone_id}"
-  name    = "${var.env_region}.${var.env_domain}"
-  type    = "CNAME"
-  ttl     = "60"
-  records = ["${aws_lb.main.dns_name}"]
-}
-
